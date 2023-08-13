f47783771f90639fa25668c46788ca6255e384c4
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-08-13 14:17:23 -0300

    Build Creating a Django Custom User Model/ChatServer/ApiDocumentation

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -31,6 +31,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "drf_spectacular",
+    "account",
+    "server",
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,18 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    # YOUR SETTINGS
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Your Project API",
+    "DESCRIPTION": "Your project description",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": True,
+    # OTHER SETTINGS
+}
