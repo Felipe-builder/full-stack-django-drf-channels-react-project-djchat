5b2c860a676d9d0ed9148fdaac43e12eaa4da7ef
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-08-15 22:54:00 -0300

    Build: Returning Related Data - Server Associated Channels

--- djchat/server/serializer.py
+++ djchat/server/serializer.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Category, Server
+from .models import Category, Channel, Server
+
+
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = "__all__"
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    channel_server = ChannelSerializer(many=True)
+
     class Meta:
         model = Server
         fields = "__all__"
