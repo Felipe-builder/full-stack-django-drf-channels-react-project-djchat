f63c767bcbcc0b7125ef1ffc19e21e6eed9fb2bd
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-08-15 22:48:31 -0300

    Build: Creating an API Endpoint for Filtering Servers by Server ID

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
 
 from .models import Server
@@ -13,6 +14,10 @@ class ServerListViewSet(viewsets.ViewSet):
         category = request.query_params.get("category")
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
+        by_serverid = request.query_params.get("by_serverid")
+
+        if by_user or by_serverid and not request.user.is_authenticated:
+            raise AuthenticationFailed
 
         if category:
             self.queryset = self.queryset.filter(category_name=category)
@@ -23,6 +28,16 @@ class ServerListViewSet(viewsets.ViewSet):
 
         if qty:
             self.queryset = self.queryset[: int(qty)]
+
+        if by_serverid:
+            try:
+                self.queryset = self.queryset.filter(id=by_serverid)
+                if not self.queryset.exists():
+                    raise ValidationError(
+                        detail=f"Server with id {by_serverid} not found"
+                    )
+            except ValueError:
+                raise ValidationError(detail=f"Server with id {by_serverid} not found")
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
