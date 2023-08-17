71908a9ae86ee0ff135898bccf714d387acec8eb
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-08-16 22:28:36 -0300

    Build: Configuring Django to Handle Storing Images

--- djchat/djchat/urls.py
+++ djchat/djchat/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
@@ -12,3 +14,6 @@ urlpatterns = [
     path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/schema/ui/", SpectacularSwaggerView.as_view()),
 ] + router.urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
