4eb5b193b5a2a5e9c1a3f9380ac344143d582af4
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-08-16 21:10:20 -0300

    Build: Creating an API Endpoint for Filtering Servers and Returning Annotation o

--- djchat/server/serializer.py
+++ djchat/server/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Category, Channel, Server
+from .models import Channel, Server
 
 
 class ChannelSerializer(serializers.ModelSerializer):
@@ -10,8 +10,21 @@ class ChannelSerializer(serializers.ModelSerializer):
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    num_members = serializers.SerializerMethodField()
     channel_server = ChannelSerializer(many=True)
 
     class Meta:
         model = Server
-        fields = "__all__"
+        exclude = ("member",)
+
+    def get_num_members(self, obj):
+        if hasattr(obj, "num_members"):
+            return obj.num_members
+        return None
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        num_members = self.context.get("num_members")
+        if not num_members:
+            data.pop("num_members", None)
+        return data
--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.db.models import Count
 from rest_framework import viewsets
 from rest_framework.exceptions import AuthenticationFailed, ValidationError
 from rest_framework.response import Response
@@ -15,6 +15,7 @@ class ServerListViewSet(viewsets.ViewSet):
         qty = request.query_params.get("qty")
         by_user = request.query_params.get("by_user") == "true"
         by_serverid = request.query_params.get("by_serverid")
+        with_num_members = request.query_params.get("with_num_members") == "true"
 
         if by_user or by_serverid and not request.user.is_authenticated:
             raise AuthenticationFailed
@@ -25,6 +26,9 @@ class ServerListViewSet(viewsets.ViewSet):
         if by_user:
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
+
+        if with_num_members:
+            self.queryset = self.queryset.annotate(num_members=Count("member"))
 
         if qty:
             self.queryset = self.queryset[: int(qty)]
@@ -39,5 +43,7 @@ class ServerListViewSet(viewsets.ViewSet):
             except ValueError:
                 raise ValidationError(detail=f"Server with id {by_serverid} not found")
 
-        serializer = ServerSerializer(self.queryset, many=True)
+        serializer = ServerSerializer(
+            self.queryset, many=True, context={"num_members": with_num_members}
+        )
         return Response(serializer.data)
