37ad7e7d24dc6ebd65f8b0cb85636039170ffda2
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-08-18 20:54:46 -0300

    Build: React Routing Introduction with React Router

--- reactchat/src/App.tsx
+++ reactchat/src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return <></>;  
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import Home from './pages/Home'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path='/' element={<Home />}/>
+    </Route>
+  )
+)
+
+const App = () => {
+  return (
+    <RouterProvider router={router} />
+  );  
 }
 
 export default App
---
+++ reactchat/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <p>home</p>
+      <p>home</p>
+    </>
+  )
+}
+
+export default Home;
