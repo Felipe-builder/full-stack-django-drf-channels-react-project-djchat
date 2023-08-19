420254084f20ff5163da73e67e86b466902653d0
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-08-19 18:02:52 -0300

    Build: Install Axios and create first API request (CORS Intro)

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     # External
     "drf_spectacular",
     "rest_framework",
+    "corsheaders",
     # Internal
     "account",
     "server",
@@ -47,6 +48,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    # "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "djchat.urls"
@@ -141,3 +144,7 @@ SPECTACULAR_SETTINGS = {
     "SERVE_INCLUDE_SCHEMA": True,
     # OTHER SETTINGS
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
--- reactchat/src/pages/templates/SecondaryDraw.tsx
+++ reactchat/src/pages/templates/SecondaryDraw.tsx
@@ -1,10 +1,18 @@
 import { Box, Typography } from "@mui/material"
 import { useTheme } from "@mui/material/styles"
 
+import axios from 'axios';
 
 
 const SecondaryDraw = () => {
   const theme = useTheme();
+
+  axios.get('http://localhost:8000/api/server/select/?category=Programa%C3%A7%C3%A3o')
+  .then(response => {
+    console.log(response.data)
+  }).catch(error => {
+    console.log(error)
+  })
 
   return (
     <Box
