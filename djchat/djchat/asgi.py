bcdc4226ad100e77f6db428aab3481da35dacc68
Felipe Vasconcelos <felipe.vasconcelos@semantix.ai>
2023-09-17 17:49:45 -0300

    adjust in websocket and enviroment py

--- djchat/djchat/asgi.py
+++ djchat/djchat/asgi.py
@@ -1,12 +1,3 @@
-"""
-ASGI config for djchat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -14,8 +5,7 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djchat.settings")
 
-
-django_applicatgion = get_asgi_application()
+django_application = get_asgi_application()
 
 from . import urls  # noqa isort:skip
 
