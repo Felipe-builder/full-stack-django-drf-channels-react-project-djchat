9122fdca52d2f77e80db29c91ebf36c49a26c28f
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-10-31 21:26:12 -0300

    feat: 67. Build: Templating - Message Template

--- reactchat/src/components/Main/MessageInterface.tsx
+++ reactchat/src/components/Main/MessageInterface.tsx
@@ -3,7 +3,7 @@ import { useWebSocket } from "react-use-websocket/dist/lib/use-websocket";
 import useCrud from "../../hooks/useCrud";
 import { Server } from "../../@types/server";
 import { useParams } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
 
 import MessageInterfaceChannels from './MessageInterfaceChannels';
 
@@ -20,7 +20,7 @@ interface Message {
 const MessageInterface = (props: ServerChannelProps) => {
   const { data } = props;
   const [newMessage, setNewMessage] = useState<Message[]>([]);
-  const [ message, setMessage ] = useState('');
+  const [message, setMessage] = useState('');
   const { serverId, channelId } = useParams();
   const server_name = data?.[0]?.name ?? "Server";
   const { fetchData } = useCrud<Server>(
@@ -29,8 +29,8 @@ const MessageInterface = (props: ServerChannelProps) => {
   );
 
   const socketUrl = channelId
-  ? `ws://127.0.0.1:8000/${serverId}/${channelId}`
-  : null;
+    ? `ws://127.0.0.1:8000/${serverId}/${channelId}`
+    : null;
 
   const { sendJsonMessage } = useWebSocket(socketUrl, {
     onOpen: async () => {
@@ -57,12 +57,12 @@ const MessageInterface = (props: ServerChannelProps) => {
 
   return (
     <>
-      <MessageInterfaceChannels data={data}/>
+      <MessageInterfaceChannels data={data} />
       {
         channelId === undefined ? (
           <Box
             sx={{
-              overflow: "hidden", 
+              overflow: "hidden",
               p: { xs: 0 },
               height: `calc(80vh)`,
               display: 'flex',
@@ -79,7 +79,7 @@ const MessageInterface = (props: ServerChannelProps) => {
                 variant="h4"
                 fontWeight={700}
                 letterSpacing={"-0.5px"}
-                sx={{ px: 5, maxWidth: '600px'}}
+                sx={{ px: 5, maxWidth: '600px' }}
               >
                 Welcome to {server_name}
               </Typography>
@@ -88,9 +88,66 @@ const MessageInterface = (props: ServerChannelProps) => {
               </Typography>
             </Box>
           </Box>
-        ): (
+        ) : (
           <>
-            <div>
+            <Box sx={{ overflow: "hidden", p: 0, height: `calc(100vh - 100px)` }}>
+              <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+                {newMessage.map((msg: Message, index: number) => {
+                  return (
+                    <ListItem key={index} alignItems="flex-start">
+                      <ListItemAvatar>
+                        <Avatar alt="user image" />
+                      </ListItemAvatar>
+                      <ListItemText
+                        primaryTypographyProps={{
+                          fontSize: "12px",
+                          variant: "body2"
+                        }}
+                        primary={
+                          <Typography
+                            component="span"
+                            variant="body1"
+                            color="text.primary"
+                            sx={{
+                              display: "inline", fontW: 600
+                            }}
+                          >
+                            {msg.sender}
+                          </Typography>
+                        }
+                        secondary={
+                          <Box>
+                            <Typography
+                              variant="body1"
+                              style={{
+                                overflow: "visible",
+                                whiteSpace: "normal",
+                                textOverflow: "clip"
+                              }}
+                              sx={{
+                                display: "inline",
+                                lineHeight: 1.2,
+                                fontWeight: 400,
+                                letterSpacing: "-0.2px"
+                              }}
+                              component="span"
+                              color="text.primary"
+                            >
+                              {msg.content}
+                            </Typography>
+                          </Box>
+                        }
+                      >
+
+                      </ListItemText>
+                    </ListItem>
+                  );
+                })}
+              </List>
+
+            </Box>
+
+            {/* <div>
               {newMessage.map((msg: Message, index: number) => {
                 return (
                   <div key={index}>
@@ -116,9 +173,9 @@ const MessageInterface = (props: ServerChannelProps) => {
               >
                 Send Message
               </button>
-            </div>
+            </div> */}
           </>
-        ) 
+        )
       }
     </>
   );
