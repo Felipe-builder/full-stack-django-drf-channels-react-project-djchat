31bddc896e4cee468a025ee5007f97e4ba12ef5a
Felipe Vasconcelos <felipesvascon@gmail.com>
2023-11-03 15:14:09 -0300

    Build: Templating - Message Scrolling

--- reactchat/src/components/Main/MessageInterface.tsx
+++ reactchat/src/components/Main/MessageInterface.tsx
@@ -7,6 +7,8 @@ import { Avatar, Box, List, ListItem, ListItemAvatar, ListItemText, TextField, T
 
 import MessageInterfaceChannels from './MessageInterfaceChannels';
 import { useTheme } from "@mui/material";
+import Scroll from "./Scroll";
+import React from "react";
 
 
 interface SendMessageData {
@@ -120,60 +122,61 @@ const MessageInterface = (props: ServerChannelProps) => {
         ) : (
           <>
             <Box sx={{ overflow: "hidden", p: 0, height: `calc(100vh - 100px)` }}>
-              <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-                {newMessage.map((msg: Message, index: number) => {
-                  return (
-                    <ListItem key={index} alignItems="flex-start">
-                      <ListItemAvatar>
-                        <Avatar alt="user image" />
-                      </ListItemAvatar>
-                      <ListItemText
-                        primaryTypographyProps={{
-                          fontSize: "12px",
-                          variant: "body2"
-                        }}
-                        primary={
-                          <Typography
-                            component="span"
-                            variant="body1"
-                            color="text.primary"
-                            sx={{
-                              display: "inline", fontW: 600
-                            }}
-                          >
-                            {msg.sender}
-                          </Typography>
-                        }
-                        secondary={
-                          <Box>
+              <Scroll>
+                <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+                  {newMessage.map((msg: Message, index: number) => {
+                    return (
+                      <ListItem key={index} alignItems="flex-start">
+                        <ListItemAvatar>
+                          <Avatar alt="user image" />
+                        </ListItemAvatar>
+                        <ListItemText
+                          primaryTypographyProps={{
+                            fontSize: "12px",
+                            variant: "body2"
+                          }}
+                          primary={
                             <Typography
-                              variant="body1"
-                              style={{
-                                overflow: "visible",
-                                whiteSpace: "normal",
-                                textOverflow: "clip"
-                              }}
-                              sx={{
-                                display: "inline",
-                                lineHeight: 1.2,
-                                fontWeight: 400,
-                                letterSpacing: "-0.2px"
-                              }}
                               component="span"
+                              variant="body1"
                               color="text.primary"
+                              sx={{
+                                display: "inline", fontW: 600
+                              }}
                             >
-                              {msg.content}
+                              {msg.sender}
                             </Typography>
-                          </Box>
-                        }
-                      >
+                          }
+                          secondary={
+                            <>
+                              <Typography
+                                variant="body1"
+                                style={{
+                                  overflow: "visible",
+                                  whiteSpace: "normal",
+                                  textOverflow: "clip"
+                                }}
+                                sx={{
+                                  display: "inline",
+                                  lineHeight: 1.2,
+                                  fontWeight: 400,
+                                  letterSpacing: "-0.2px"
+                                }}
+                                component="span"
+                                color="text.primary"
+                              >
+                                {msg.content}
+                              </Typography>
+                            </>
+                          }
+                        >
 
-                      </ListItemText>
-                    </ListItem>
-                  );
-                })}
-              </List>
-
+                        </ListItemText>
+                      </ListItem>
+                    );
+                  })}
+                </List>
+              </Scroll>
             </Box>
             <Box sx={{
               position: "sticky",
